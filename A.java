5ccf4f9a52d808ffbfd6067d4fe0ead5d0ee7ae8
akmal <53895835+akmal42@users.noreply.github.com>
2022-09-14 19:53:31 +0530

    first logical coding
    first logical coding

---
+++ A.java
@@ -0,0 +1,24 @@
+/*
+ * 1111111111
+ * 1111111111
+ * 1111111111
+ * 1111111111
+ * 1111111111
+ * 1111111111
+ * 1111111111
+ * 1111111111
+ * 1111111111
+ * 1111111111
+ */
+public class A {
+	public static void main(String [] args) {
+		for(int j = 1 ; j <= 10 ; j++)
+		{
+			for(int i = 1 ; i<=10; i++)
+			{
+				System.out.print(1);
+			}
+			System.out.println();
+		}
+	}
+}
